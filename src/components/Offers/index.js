c09c4a4cc18623db7b294d38173ccc06035929da
AN <shriarunnanda@gmail.com>
2021-09-12 14:53:30 +0530

    offers page revamp started

--- src/components/Offers/index.js
+++ src/components/Offers/index.js
@@ -1,13 +1,23 @@
 import React, {useContext, useState} from 'react';
 import styles from './styles';
-import {Alert, Linking, Text, TouchableOpacity, View} from 'react-native';
+import {
+  Alert,
+  FlatList,
+  Linking,
+  Pressable,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+import Icon from '../../components/common/Icon';
 import {GlobalContext} from '../../context/Provider';
-import FloatingCenterButton from '../common/FloatingCenterButton';
+import FloatingLeftButton from '../common/FloatingLeftButton';
 import {useNavigation} from '@react-navigation/native';
 import colors from '../../assets/theme/colors';
 import {CUSTOMERS, PROFILE} from '../../constants/routeNames';
 import AddCustomer from '../AddCustomer';
 import CustomButtonMedium from '../common/CustomButtonMedium';
+import FloatingRightButton from '../common/FloatingRightButton';
 
 const OffersComponent = () => {
   const {authState} = useContext(GlobalContext);
@@ -15,25 +25,151 @@ const OffersComponent = () => {
   const [modalVisibleAddCustomer, setModalVisibleAddCustomer] = useState(false);
   let customerView;
 
+  const offersData = [
+    {
+      id: 1,
+      name: 'foo',
+      quantity: '100g',
+      regularPrice: '100',
+      offerPrice: '80',
+      active: true,
+    },
+    {
+      id: 2,
+      name: 'bar',
+      quantity: '100g',
+      regularPrice: '200',
+      offerPrice: '180',
+      active: true,
+    },
+  ];
+
+  const ListHeaderComponent = () => {
+    return (
+      <View>
+        <View style={styles.columnHeaders}>
+          <Text style={styles.availabilityInfo}> </Text>
+          <Text style={styles.itemTitle}>Item</Text>
+          <Text style={styles.quantityTitle}>Qty </Text>
+          <Text style={styles.priceTitle}>Price</Text>
+          <Text style={styles.priceTitle}>Offer</Text>
+          {/* <Text style={styles.priceTitle}> </Text> */}
+        </View>
+      </View>
+    );
+  };
+
+  const renderItem = ({item}) => {
+    const {
+      id: offerItemId,
+      name: itemName,
+      quantity: itemQuantity,
+      regularPrice: regularPrice,
+      offerPrice: offerPrice,
+      active: offerActive,
+      // customerComment: itemCustomerComment,
+    } = item;
+    console.log(
+      'in offer items component render item. item id is:  > ',
+      offerItemId,
+    );
+
+    return (
+      <View style={styles.listItem}>
+        <Pressable
+          onPress={() => {
+            // setCurrentItem(item);
+            // setModalVisibleViewItem(true);
+            // setCurrentItemId(orderItemId);
+            console.log('-----------------in offers component. ');
+          }}>
+          <View>
+            <View style={styles.listRowItem}>
+              <Text style={styles.availabilityInfo}>
+                {offerActive !== null ? (
+                  offerActive === true ? (
+                    <Icon
+                      type="fontAwesome"
+                      name="check"
+                      style={styles.checkMark}
+                    />
+                  ) : (
+                    <Icon
+                      type="fontAwesome"
+                      name="remove"
+                      style={styles.crossMark}
+                    />
+                  )
+                ) : (
+                  '?'
+                )}
+              </Text>
+
+              <Text style={styles.itemInfo}>{itemName}</Text>
+              <Text style={styles.quantityInfo}> {itemQuantity}</Text>
+              <Text style={styles.priceInfo}>
+                {' '}
+                {regularPrice && (
+                  <Text>
+                    {'\u20B9'} {regularPrice}
+                  </Text>
+                )}
+              </Text>
+              <Text style={styles.priceInfo}>
+                {' '}
+                {offerPrice && (
+                  <Text>
+                    {'\u20B9'} {offerPrice}
+                  </Text>
+                )}
+              </Text>
+            </View>
+          </View>
+        </Pressable>
+      </View>
+    );
+  };
+
   if (authState.latitude && authState.longitude) {
     customerView = (
       <>
         <View style={styles.container}>
-          <Text style={styles.loadingText}>
-            Special offers for your customers.
+          <Text style={styles.headerText}>
+            Make special offers for your customers.
           </Text>
           <View styles={styles.loadingTextIndicator}>
-            <Text style={styles.loadingText}>
+            {/* <Text style={styles.loadingText}>
               You will be eligible to make special offers after doing regular
               business for 3 months.
-            </Text>
-            <Text style={styles.loadingText}>
-              We will sponsor and promote offers for customers of high ranked
-              stores.
+            </Text> */}
+            <Text style={styles.headerText}>
+              We will promote offers for customers of high ranked stores.
             </Text>
           </View>
         </View>
-        <FloatingCenterButton
+
+        <FlatList
+          style={styles.list}
+          data={offersData}
+          extraData={offersData.map(x => x.active)}
+          keyExtractor={item => String(item.id)}
+          renderItem={renderItem}
+          ListHeaderComponent={ListHeaderComponent}
+          // ListFooterComponent={ListFooterComponent}
+          // ListEmptyComponent={ListEmptyComponent}
+          // ItemSeparatorComponent={ListItemSeparatorComponent}
+        />
+
+        <FloatingLeftButton
+          buttonText="Add offer"
+          iconType="fa"
+          iconName="plus"
+          circleColor={colors.color2_2_4}
+          iconColor={colors.color1_3}
+          onPress={() => {}}
+        />
+
+        <FloatingRightButton
           buttonText="Broadcast"
           iconType="fa"
           iconName="bullhorn"
--- src/components/Offers/styles.js
+++ src/components/Offers/styles.js
@@ -3,14 +3,20 @@ import colors from '../../assets/theme/colors';
 
 export default ScaledSheet.create({
   container: {
-    paddingVertical: '30@s',
-    paddingHorizontal: '30@s',
+    paddingVertical: '10@s',
+    paddingHorizontal: '10@s',
   },
   loadingText: {
     fontSize: '22@s',
     color: colors.color4_2,
     paddingVertical: '20@s',
   },
+  headerText: {
+    fontSize: '16@s',
+    color: colors.color4_2,
+    paddingVertical: '5@s',
+  },
+
   loadingTextIndicator: {
     flexDirection: 'row',
   },
@@ -34,5 +40,75 @@ export default ScaledSheet.create({
   buttonSection: {
     paddingHorizontal: '100@s',
     paddingBottom: '10@s',
+  },
+  columnHeaders: {
+    flexDirection: 'row',
+    paddingTop: '8@s',
+  },
+  itemTitle: {
+    fontFamily: 'sans-serif-smallcaps',
+    flex: 5,
+    fontSize: '21@s',
+    fontWeight: '700',
+  },
+  quantityTitle: {
+    fontFamily: 'sans-serif-smallcaps',
+    flex: 2.25,
+    fontSize: '21@s',
+    fontWeight: '700',
+  },
+  priceTitle: {
+    fontFamily: 'sans-serif-smallcaps',
+    fontSize: '21@s',
+    flex: 2.25,
+    flexWrap: 'wrap',
+  },
+  list: {
+    // paddingHorizontal: '10@s',
+    // paddingTop: '10@s',
+    backgroundColor: colors.color2_3,
+  },
+  listItem: {
+    paddingHorizontal: '10@s',
+  },
+  listRowItem: {
+    flexDirection: 'row',
+    paddingVertical: 9,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  itemInfo: {
+    fontFamily: 'Tillana-Medium',
+    fontSize: '18@s',
+    flex: 5,
+    flexWrap: 'wrap',
+  },
+  quantityInfo: {
+    fontFamily: 'Tillana-Medium',
+    fontSize: '18@s',
+    flex: 2.25,
+    flexWrap: 'wrap',
+  },
+  priceInfo: {
+    fontFamily: 'Tillana-Medium',
+    fontSize: '18@s',
+    flex: 2.25,
+    flexWrap: 'wrap',
+  },
+  availabilityInfo: {
+    // fontFamily: 'Tillana-Medium',
+    flex: 0.75,
+    flexWrap: 'wrap',
+    fontSize: '20@s',
+    color: 'red',
+    fontWeight: '700',
+  },
+  checkMark: {
+    color: colors.color4_1,
+    fontSize: '21@s',
+  },
+  crossMark: {
+    color: colors.color3_1,
+    fontSize: '21@s',
   },
 });
